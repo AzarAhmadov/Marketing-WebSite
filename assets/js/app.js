1096e39831ced419244a9c481c6a77567dc4343e
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2022-03-15 09:27:32 -0700

    duzenlemeler

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,36 @@
 var swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-      el: ".swiper-pagination",
-      dynamicBullets: true,
+  slidesPerView: 3,
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    140: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+    1224: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
 });
+
+const header = document.querySelector('.header')
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 400) {
+    header.classList.add('active')
+  } else {
+    header.classList.remove('active')
+  }
+})
